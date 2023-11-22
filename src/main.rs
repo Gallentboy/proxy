99f5033a6d7c4ef948123bf74c10a7670436c952
zhangyue <15527527763@163.com>
2023-11-22 15:13:15 +0800

    增加功能

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
+use std::cell::RefCell;
+use std::collections::HashSet;
 use std::convert::Infallible;
 use std::net::SocketAddr;
 use std::process::exit;
 use std::str::FromStr;
+use std::sync::Mutex;
 
-use hyper::{Body, Client, HeaderMap, Method, Request, Response, Server, StatusCode, Uri};
+use hyper::{Body, Client, HeaderMap, Request, Response, Server, StatusCode, Uri};
+use hyper::body::HttpBody;
+use hyper::body::Buf;
 use hyper::header::{HeaderName, HeaderValue};
 use hyper::service::{make_service_fn, service_fn};
 use hyper_tls::HttpsConnector;
@@ -11,6 +16,7 @@ use lazy_static::lazy_static;
 
 type HttpClient = Client<hyper::client::HttpConnector>;
 type HttpsClient = Client<HttpsConnector<hyper::client::HttpConnector>>;
+type GenericError = Box<dyn std::error::Error + Send + Sync>;
 
 const PROXY_TARGET: &str = "x-proxy-target";
 const DEFAULT_PORT: u16 = 4225;
@@ -31,9 +37,21 @@ lazy_static! {
     static ref HTTP_CLIENT: HttpClient = build_http_client();
 
     static ref HTTPS_CLIENT : HttpsClient = build_https_client();
+
+    static ref OPTIONS_HEADERS: Mutex<RefCell<HashSet<String>>> = Mutex::new(RefCell::new(HashSet::new()));
 }
 
+fn init_options_headers() {
+    OPTIONS_HEADERS.lock().unwrap().borrow_mut().insert(PROXY_TARGET.to_string());
+}
+
+fn add_options_headers(src: Vec<String>) {
+    OPTIONS_HEADERS.lock().unwrap().borrow_mut().extend(src)
+}
+
+
 fn main() {
+    init_options_headers();
     let args: Vec<String> = std::env::args()
         .skip(1)
         .collect();
@@ -53,10 +71,23 @@ fn main() {
         .enable_time()
         .build()
         .unwrap();
-    let enter_guard = rt.enter();
+    let _enter_guard = rt.enter();
     let addr = SocketAddr::from(([127, 0, 0, 1], port));
     let make_service = make_service_fn(move |_| {
-        async move { Ok::<_, Infallible>(service_fn(move |req| proxy(req))) }
+        async move {
+            Ok::<_, Infallible>(service_fn(move |req| async {
+                let uri = req.uri().to_string();
+                let res = proxy(req).await.unwrap_or_else(|e| {
+                    let s = format!("请求[{}]处理失败: {}", uri, e.to_string());
+                    Response::builder()
+                        .status(500)
+                        .body(Body::from(s))
+                        .unwrap()
+                });
+                Ok(res) as Result<Response<Body>, GenericError>
+            }
+            ))
+        }
     });
 
     let server = Server::try_bind(&addr)
@@ -80,25 +111,66 @@ fn main() {
     }
 }
 
-async fn proxy(req: Request<Body>) -> Result<Response<Body>, String> {
+fn get_options_headers(headers: &str) -> String {
+    let s = OPTIONS_HEADERS.lock().unwrap()
+        .borrow()
+        .iter()
+        .map(|it| it.as_str())
+        .collect::<Vec<&str>>()
+        .join(", ");
+    if headers.is_empty() {
+        return s;
+    }
+    let vec = vec![s, headers.to_string(), "content-type".to_string()];
+    let mut set = HashSet::new();
+    set.extend(vec);
+    set.iter().map(|it| it.as_str()).collect::<Vec<&str>>().join(", ")
+}
+
+async fn handle_add_options_header_request(req: Request<Body>) -> Result<Response<Body>, GenericError> {
+    let origin = get_origin(req.headers()).unwrap_or("*".to_string());
+    let method = req.method().as_str().to_string();
+    let whole_body = hyper::body::aggregate(req).await?;
+    let data: Vec<String> = serde_json::from_reader(whole_body.reader())?;
+    add_options_headers(data);
+    let mut response = Response::builder()
+        .status(204)
+        .body(Body::empty())?;
+    modify_response_headers(response.headers_mut(),
+                            &origin,
+                            &method,
+                            "");
+    Ok(response)
+}
+
+async fn proxy(req: Request<Body>) -> Result<Response<Body>, GenericError> {
     let origin = get_origin(req.headers()).unwrap_or("*".to_string());
     let exposed_headers = get_exposed_headers(req.headers());
     let method = req.method().as_str().to_string();
+    let allow_headers = req.headers()
+        .iter()
+        .map(|(k, _)| k.as_str()).collect::<Vec<&str>>().join(", ");
     if method.eq_ignore_ascii_case("options") {
         let mut response = Response::new(Body::empty());
         let headers = response.headers_mut();
         headers.insert(HeaderName::from_static("access-control-allow-headers"),
-                       HeaderValue::from_static(PROXY_TARGET));
+                       HeaderValue::from_str(&get_options_headers(&allow_headers)).unwrap());
         headers.insert(HeaderName::from_static("access-control-allow-origin"),
                        HeaderValue::from_str(origin.as_str()).unwrap());
         headers.insert(HeaderName::from_static("access-control-allow-credentials"),
                        HeaderValue::from_static("true"));
         headers.insert(HeaderName::from_static("access-control-allow-methods"),
-                       HeaderValue::from_static("GET, POST, PUT, PATCH, DELETE"));
+                       // HeaderValue::from_static("GET, POST, PUT, PATCH, DELETE"));
+                       HeaderValue::from_static("POST"));
         let (mut parts, body) = response.into_parts();
-        parts.status = StatusCode::NO_CONTENT;
+        parts.status = StatusCode::OK;
         return Ok(Response::from_parts(parts, body));
     }
+
+    if req.uri().path().ends_with("/addOptionsHeaders") {
+        return handle_add_options_header_request(req).await;
+    }
+
     let proxy_target;
     match get_proxy_target(req.headers()) {
         None => {
@@ -113,7 +185,7 @@ async fn proxy(req: Request<Body>) -> Result<Response<Body>, String> {
                     Ok(res)
                 }
                 Err(e) => {
-                    Err(e.to_string())
+                    Err(GenericError::from(e))
                 }
             };
         }
@@ -141,7 +213,7 @@ async fn proxy(req: Request<Body>) -> Result<Response<Body>, String> {
                                 exposed_headers);
         it
     })
-        .map_err(|e| e.to_string())
+        .map_err(|e| GenericError::from(e))
 }
 
 fn show_headers(headers: &HeaderMap<HeaderValue>) {
@@ -193,7 +265,7 @@ fn get_origin(headers: &HeaderMap) -> Option<String> {
 #[inline(always)]
 fn get_exposed_headers(headers: &HeaderMap) -> String {
     headers.iter()
-        .filter(|(k, _)| !k.as_str().eq_ignore_ascii_case(PROXY_TARGET) )
+        .filter(|(k, _)| !k.as_str().eq_ignore_ascii_case(PROXY_TARGET))
         .map(|(k, _v)| k.as_str())
         .collect::<Vec<&str>>()
         .join(", ")
